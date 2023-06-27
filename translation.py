9347833dd40101c633bb47e0e53f2109b144be72
tuearnings <136893874+tuearnings@users.noreply.github.com>
2023-06-27 18:46:41 +0530

    Update translation.py

--- translation.py
+++ translation.py
@@ -13,6 +13,21 @@ I'll handle the rest and get those links shortened or converted in a short time!
 
 START_MESSAGE = """Hi there {} 
 
+step 1:- select your shortner 👇👇
+`/base_site tulinks.online`
+`/base_site tulinks.one`
+
+2. Go To 👉 https://tulinks.online/member/tools/api
+3. Than Copy API Key
+4. Than Type /api than give a single space and than paste your API Key (see example to understand more...)
+
+➕ Hit 👉 /help To Get Help.
+➕ Hit 👉 /footer To Get Help About Adding your Custom Footer to bot.
+
+- Made With 🤍 By @tu_earnings_supoort -
+
+| தமிழ் | తెలుగు | हिंदी | മലയാളം | ಕನ್
+
 To add or update your tulinks shortner api see example 👇👇
 
 /shortener_api 6LZq851sXofffPHugiKQq
