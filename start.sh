aa1d1b03bc08f43ae6e632ef9869d7a54ae251dd
tuearnings <136893874+tuearnings@users.noreply.github.com>
2024-01-24 15:15:01 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $SOURCE_CODE ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/kiran6412/Readytuonline.git /URL-Shortener-V2backup
+  git clone https://github.com/kiran6412/Readytuonline.git /Readytuonline
 else
   echo "Cloning Custom Repo from $SOURCE_CODE "
   git clone $SOURCE_CODE /Readytuonline
