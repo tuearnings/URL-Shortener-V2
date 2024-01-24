38a671c4022980e42f9a64b0cd175479639462ca
tuearnings <136893874+tuearnings@users.noreply.github.com>
2024-01-24 15:05:10 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $SOURCE_CODE ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/kiran6412/URL-Shortener-V2backup.git /URL-Shortener-V2backup
+  git clone https://github.com/kiran6412/Readytuonline.git /URL-Shortener-V2backup
 else
   echo "Cloning Custom Repo from $SOURCE_CODE "
-  git clone $SOURCE_CODE /URL-Shortener-V2backup
+  git clone $SOURCE_CODE /Readytuonline
 fi
 cd /URL-Shortener-V2backup
 pip3 install -U -r requirements.txt
